a8cbfbca3248f3e5d985487da8bf7229fe799b3a
yuneKim <yune@yunekim.dev>
2023-12-25 19:00:34 -0500

    trying to fix the github pages being blank. changed the base url on vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'pinia-task-demo',
+  base: '/pinia-tasks-demo/',
   // build: {
   //   outDir: 'docs',
   // },
